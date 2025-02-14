59a9657ec35d5fd2d703bbeca6cdf714f9a8638c
Papaseit <alejandropr2005@gmail.com>
2025-02-14 09:07:45 +0100

    Tienda app

--- src/main/java/org/example/tiendaspringboot/TiendaSpringbootApplication.java
+++ src/main/java/org/example/tiendaspringboot/TiendaSpringbootApplication.java
@@ -2,10 +2,11 @@ package org.example.tiendaspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TiendaSpringbootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TiendaSpringbootApplication.class, args);
     }
