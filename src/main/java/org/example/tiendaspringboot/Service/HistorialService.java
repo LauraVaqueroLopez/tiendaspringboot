1c0081454eb7e969a813300b3f0bc511cef30ec4
Papaseit <alejandropr2005@gmail.com>
2025-02-24 08:37:11 +0100

    final

--- src/main/java/org/example/tiendaspringboot/Service/HistorialService.java
+++ src/main/java/org/example/tiendaspringboot/Service/HistorialService.java
@@ -44,13 +44,11 @@ public class HistorialService {
         Producto producto = optionalProducto.get();
         Cliente cliente = optionalCliente.get();
 
-        // Obtener todas las compras y devoluciones previas del cliente para este producto
         List<Historial> historialCliente = historialRepository.findAll();
 
         int cantidadComprada = 0;
         int cantidadDevuelta = 0;
 
-        // Calcular la cantidad total comprada y devuelta por el cliente para este producto
         for (Historial h : historialCliente) {
             if (h.getCliente().getId().equals(cliente.getId()) && h.getProducto().getId().equals(producto.getId())) {
                 if ("COMPRA".equalsIgnoreCase(h.getTipo())) {
@@ -62,27 +60,19 @@ public class HistorialService {
         }
 
         if ("COMPRA".equalsIgnoreCase(historial.getTipo())) {
-            // Verificar si hay suficiente stock para la compra
             if (historial.getCantidad() > producto.getStock()) {
                 return "No hay stock suficiente";
             }
-            // Descontar del stock
             producto.setStock(producto.getStock() - historial.getCantidad());
 
         } else if ("DEVOLUCION".equalsIgnoreCase(historial.getTipo())) {
-            // Verificar si el cliente intenta devolver más productos de los que ha comprado
             if (cantidadDevuelta + historial.getCantidad() > cantidadComprada) {
                 return "No puedes devolver más productos de los que compraste";
             }
-
-            // Incrementar el stock en una devolución válida
             producto.setStock(producto.getStock() + historial.getCantidad());
-
         } else {
             return "Tipo de operación no válida (Debe ser COMPRA o DEVOLUCION)";
         }
-
-        // Guardar el historial y actualizar el producto
         historialRepository.save(historial);
         productosRepository.save(producto);
 
